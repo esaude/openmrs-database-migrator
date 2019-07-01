8ef77decefab53d1b41055a6ba01e43b542c0a7c
Nathan Floor <nathan.floor@jembi.org>
2019-07-01 20:11:34 +0200

    Added some example templates and layout for the app

--- src/main/java/com/openmrs/migrator/controllers/HomeController.java
+++ src/main/java/com/openmrs/migrator/controllers/HomeController.java
@@ -6,12 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class MigratorController {
+public class HomeController {
 
-    @GetMapping("/migrator")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "views/greeting";
+    @GetMapping("/")
+    public String index() {
+        return "views/index";
     }
 
 }
