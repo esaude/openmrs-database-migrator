99662ee659977dc1a5bfd037512cd632bc435429
Nathan Floor <nathan.floor@jembi.org>
2019-07-02 08:49:56 +0200

    renamed readme file and removed commented code from Application class

--- src/main/java/com/openmrs/migrator/MigratorApplication.java
+++ src/main/java/com/openmrs/migrator/MigratorApplication.java
@@ -3,17 +3,10 @@ package com.openmrs.migrator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import java.util.logging.Logger;
-// import java.util.logging.LoggerFactory;
-
 @SpringBootApplication
 public class MigratorApplication {
 
-  // private static Logger logger = LoggerFactory.getLogger(MigratorApplication.class);
-
   public static void main(String[] args) {
-    // logger.info("STARTING THE MIGRATOR...");
-    SpringApplication.run(MigratorApplication.class, args);
-    // logger.info("SETUP COMPLETE...");
+	  SpringApplication.run(MigratorApplication.class, args);
   }
 }
