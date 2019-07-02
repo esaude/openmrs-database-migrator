1c369bcd7caa4835821d74d45b7ca87c8ff32de7
Nathan Floor <nathan.floor@jembi.org>
2019-07-02 17:46:50 +0200

    Updated gitignore and added commandline runner to application

--- src/main/java/com/openmrs/migrator/MigratorApplication.java
+++ src/main/java/com/openmrs/migrator/MigratorApplication.java
@@ -1,12 +1,26 @@
 package com.openmrs.migrator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MigratorApplication {
+public class MigratorApplication implements CommandLineRunner {
+
+  private static Logger LOG = LoggerFactory.getLogger(MigratorApplication.class);
 
   public static void main(String[] args) {
-	  SpringApplication.run(MigratorApplication.class, args);
+    SpringApplication.run(MigratorApplication.class, args);
+  }
+
+  @Override
+  public void run(String... args) {
+    LOG.info("EXECUTING : command line runner");
+
+    for (int i = 0; i < args.length; ++i) {
+      LOG.info("args[{}]: {}", i, args[i]);
+    }
   }
 }
