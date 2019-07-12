748c039018e3f4a9669523e6ab905bb2f17c4f13
cfaife <clerio.faife@gmail.com>
2019-07-12 09:18:36 +0200

    MEPTS-16:  removed  the functionality that  passes the config from  resources to  ~/.kettle at  PDIServiceImpl

--- src/main/java/com/openmrs/migrator/core/services/impl/PDIServiceImpl.java
+++ src/main/java/com/openmrs/migrator/core/services/impl/PDIServiceImpl.java
@@ -2,15 +2,10 @@ package com.openmrs.migrator.core.services.impl;
 
 import com.openmrs.migrator.core.services.PDIService;
 import com.openmrs.migrator.core.utilities.FileIOUtilities;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+ 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Properties;
+ 
 import org.pentaho.di.core.KettleEnvironment;
 import org.pentaho.di.core.Result;
 import org.pentaho.di.core.exception.KettleException;
@@ -29,16 +24,12 @@ public class PDIServiceImpl implements PDIService {
 
   private final String[] transformations = {"pdiresources/transformations/merge-patient.ktr"};
 
-  private final String KETTLE_PROPERTIES = "kettle.properties";
-
-  private final String KETTLE_DIR = ".kettle";
-
   @Autowired private FileIOUtilities fileIOUtilities;
 
   @Override
   public void runTransformation(InputStream xmlStream) throws KettleException {
 
-    this.loadProperties();
+  
 
     KettleEnvironment.init();
 
@@ -80,67 +71,10 @@ public class PDIServiceImpl implements PDIService {
       // Do nothing kettle prints stack trace
     }
   }
-
-  private void loadProperties() {
-
-    try (InputStream input = getClass().getResourceAsStream("/pdiresources/" + KETTLE_PROPERTIES)) {
-
-      Properties prop = new Properties();
-
-      LOG.info(
-          "Loading connection details from "
-              + KETTLE_PROPERTIES
-              + " file in the classpath to system properties");
-
-      prop.load(input);
-
-      LOG.info(
-          "Writing properties to <user home directory>/"
-              + KETTLE_DIR
-              + "/"
-              + KETTLE_PROPERTIES
-              + " file");
-
-      File kettleFile = getKettlePropertiesLocation();
-
-      writePropertiesToKettlePropertiesOutter(kettleFile, prop);
-
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  
+  private void readJobsInPDIResources() {
+	  
   }
 
-  private void writePropertiesToKettlePropertiesOutter(File file, Properties properties)
-      throws IOException {
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(file))) {
-
-      bw.write("ETL_SOURCE_DATABASE=" + properties.getProperty("ETL_SOURCE_DATABASE"));
-      bw.newLine();
-      bw.write("ETL_DATABASE_HOST=" + properties.getProperty("ETL_DATABASE_HOST"));
-      bw.newLine();
-      bw.write("ETL_DATABASE_PORT=" + properties.getProperty("ETL_DATABASE_PORT"));
-      bw.newLine();
-      bw.write("ETL_DATABASE_USER=" + properties.getProperty("ETL_DATABASE_USER"));
-      bw.newLine();
-      bw.write("ETL_DATABASE_PASSWORD=" + properties.getProperty("ETL_DATABASE_PASSWORD"));
-    }
-  }
-
-  private File getKettlePropertiesLocation() throws IOException {
-    String homeDirectory = System.getProperty("user.home");
-
-    Path kettleDir = Paths.get(homeDirectory + "/" + KETTLE_DIR);
-    Path kettleFile = Paths.get(kettleDir + "/" + KETTLE_PROPERTIES);
-
-    if (!Files.exists(kettleDir)) {
-      Files.createDirectories(kettleDir);
-      Files.createFile(kettleFile);
-    }
-    if (!Files.exists(kettleFile)) {
-      Files.createFile(kettleFile);
-    }
-
-    File file = new File(homeDirectory + "/" + KETTLE_DIR + "/" + KETTLE_PROPERTIES);
-    return file;
-  }
+ 
 }
