3489a9aefe87778d92ad23d369c3cacd90d1b95b
Nathan Floor <nathan.floor@jembi.org>
2019-07-19 16:01:54 +0200

    Mepts 85 refactor (#6)
    * Removed PDIreosurces from base level of repo (should only be under resources folder)
    
    * MOved exception file to exceptions folder and moved delete Folders logic to FileIOUtilities class
    
    * Refactored bootstrap service by moving file IO operations to the utilities class and adding logic to copy files from resources to file directory
    
    * Updated the tests for bootstrap service and fileIO utils class.
    
    * Added unit tests for new fileIO util functions for removing directories
    
    * Removed try catches from fileIO util fucntions since the exceptions should propogate and be caught further up the chain
    
    * Added tests for remaining fileIO functions and updated existing ones to pass again
    
    * resolved codacy issues
    
    * Updated migrationApplication integration test to clean up files properly
    
    * Fixed last codacy issue - need to find a way of working around the lambda function exception warnings

--- src/main/java/com/openmrs/migrator/core/services/impl/BootstrapServiceImpl.java
+++ src/main/java/com/openmrs/migrator/core/services/impl/BootstrapServiceImpl.java
@@ -1,17 +1,14 @@
 package com.openmrs.migrator.core.services.impl;
 
+import com.openmrs.migrator.core.exceptions.InvalidParameterException;
 import com.openmrs.migrator.core.services.BootstrapService;
+import com.openmrs.migrator.core.utilities.FileIOUtilities;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,87 +16,64 @@ public class BootstrapServiceImpl implements BootstrapService {
 
   private static Logger log = LoggerFactory.getLogger(BootstrapServiceImpl.class);
 
-  private final Path TARGET_JOB_FOLDER = Paths.get("pdiresources/jobs/");
+  private FileIOUtilities fileIOUtilities;
 
-  private final Path TARGET_TRANSFORMATION_FOLDER = Paths.get("pdiresources/transformations/");
+  @Autowired
+  public BootstrapServiceImpl(FileIOUtilities fileIOUtils) {
+    this.fileIOUtilities = fileIOUtils;
+  }
 
   /**
    * Creates folders structure. If one of the folders exists, a warn log is raised informing the
    * folder won't be created because it already exists
    *
-   * @return Stream of created files
+   * @return boolean indicating success or failure
    */
   @Override
-  public Stream<String> createDirectoryStructure(List<String> dirList, Path settingsProperties)
-      throws IOException {
-    List<String> directoryStream = new ArrayList<>();
+  public boolean createDirectoryStructure(List<String> dirList) throws IOException {
+    log.info("Creating folder structure");
 
-    log.info("Starting creating folder structure");
-
-    dirList.forEach(
-        dir -> {
-          if (!checkIfPathExist(Paths.get(dir))) {
-            try {
-              Files.createDirectory(Paths.get(dir));
-              log.info("Folder: " + dir + " created sucessfully");
-              if ("pdiresources/".equals(dir)) {
-                Files.createDirectory(TARGET_JOB_FOLDER);
-                Files.createDirectory(TARGET_TRANSFORMATION_FOLDER);
-              }
-              directoryStream.add(dir);
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          } else {
-            log.warn("Folder: " + dir + " will not be created, folder already exists");
-          }
-        });
-
-    if (!checkIfPathExist(settingsProperties)) {
-      Files.createFile(settingsProperties);
-      log.info("File: settings.properties created sucessfully");
-      directoryStream.add(settingsProperties.getFileName().toString());
-    } else {
-      log.warn("File: settings.properties will not be created file already exists");
+    for (String dir : dirList) {
+      // Not the cleanest approach
+      // TODO: we should write a wrapper class that will handle this for us
+      // An option is to use a functional interface for this:
+      // https://www.baeldung.com/java-lambda-exceptions
+      try {
+        fileIOUtilities.createDirectory(Paths.get(dir));
+      } catch (IOException ex) {
+        log.error("An IO exception occurred while creating directories", ex);
+        return false;
+      }
     }
 
-    return directoryStream.stream();
-  }
-
-  private boolean checkIfPathExist(Path path) {
-
-    return Files.exists(path);
+    return true;
   }
 
   /**
    * this method copies resources of the app to folder where the app is running
    *
-   * @return Set of created files
+   * @return boolean indicating success or failure
    */
   @Override
-  public Set<String> populateDefaultResource(Set<String> sourceFiles) throws IOException {
+  public boolean populateDefaultResources(List<String> sourceFiles) throws IOException {
+    log.info("Populating PDI folders with default resources");
 
-    Set<String> createdFiles = new HashSet<>();
+    for (String pdiFile : sourceFiles) {
+      // Not the cleanest approach
+      // TODO: we should write a wrapper class that will handle this for us
+      // An option is to use a functional interface for this:
+      // https://www.baeldung.com/java-lambda-exceptions
+      try {
+        fileIOUtilities.copyFileFromResources(pdiFile);
+      } catch (IOException ex) {
+        log.error("An IOException occurred while copying resource files", ex);
+        return false;
+      } catch (InvalidParameterException paramEx) {
+        log.error("An Invalid Parameter Exception occurred while copying resource files", paramEx);
+        return false;
+      }
+    }
 
-    log.info("Starting populating PDi folders  ");
-
-    sourceFiles.forEach(
-        pdiFile -> {
-          Path pdiPath = Paths.get(pdiFile);
-          if (Files.notExists(Paths.get(pdiFile))) {
-
-            try {
-              Files.createFile(pdiPath);
-              log.info("File:" + pdiPath.getFileName() + " copied to " + pdiPath.getParent());
-              createdFiles.add(pdiFile);
-            } catch (IOException e) {
-              e.printStackTrace();
-            }
-          } else {
-            log.warn("File:" + pdiPath.getFileName() + " already exist in " + pdiPath.getParent());
-          }
-        });
-
-    return createdFiles;
+    return true;
   }
 }
--- src/main/java/com/openmrs/migrator/core/utilities/FileIOUtilities.java
+++ src/main/java/com/openmrs/migrator/core/utilities/FileIOUtilities.java
@@ -1,20 +1,29 @@
 package com.openmrs.migrator.core.utilities;
 
+import com.openmrs.migrator.core.exceptions.EmptyFileException;
+import com.openmrs.migrator.core.exceptions.InvalidParameterException;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Component
 public class FileIOUtilities {
+
+  private static Logger logger = LoggerFactory.getLogger(FileIOUtilities.class);
   private static final String UPLOADED_FOLDER = "~";
 
   public void UploadFile(MultipartFile file) throws EmptyFileException {
     if (file.isEmpty()) {
-      throw new EmptyFileException();
+      throw new EmptyFileException(file.getOriginalFilename());
     }
 
     try {
@@ -42,12 +51,109 @@ public class FileIOUtilities {
     return resourceAsStream;
   }
 
-  private class EmptyFileException extends Exception {
+  /**
+   * Check if a path exists
+   *
+   * @param path
+   * @return
+   */
+  public boolean checkIfPathExist(Path path) {
+    return Files.exists(path);
+  }
 
-    private static final long serialVersionUID = 1L;
+  /**
+   * Create a directory
+   *
+   * @param directoryToCreate
+   * @return boolean indicating if directory has been created successfully or not
+   * @throws IOException
+   */
+  public boolean createDirectory(Path directoryToCreate) throws IOException {
+    if (!checkIfPathExist(directoryToCreate)) {
+      Files.createDirectory(directoryToCreate);
+      logger.info("Folder: " + directoryToCreate + " created sucessfully");
 
-    EmptyFileException() {
-      super("File is empty");
+      return true;
+    } else {
+      logger.warn("Folder: " + directoryToCreate + " will not be created, folder already exists");
+      return false;
     }
+  }
+
+  /**
+   * Create a new file by passing in a Path Object
+   *
+   * @param fileName
+   * @return boolean indicating if file has been created successfully or not
+   * @throws IOException
+   */
+  public boolean createFile(Path fileName) throws IOException {
+    if (!checkIfPathExist(fileName)) {
+      Files.createFile(fileName);
+      logger.info("File: " + fileName + " created successfully");
+      return true;
+    } else {
+      logger.warn("File: " + fileName + " will not be created since file already exists");
+      return false;
+    }
+  }
+
+  /**
+   * Copy a file from local resources folder to same directory that the app is running in
+   *
+   * @param resourceFile
+   * @throws IOException
+   * @throws InvalidParameterException
+   */
+  public void copyFileFromResources(String resourceFile)
+      throws IOException, InvalidParameterException {
+    if (resourceFile == null || resourceFile.isEmpty()) {
+      throw new InvalidParameterException(resourceFile);
+    }
+
+    // read the files form the resources folder in the jar application
+    InputStream resourceStream = getResourceAsStream(resourceFile);
+
+    // copy files from resources to home directory
+    Files.copy(resourceStream, Paths.get(resourceFile), StandardCopyOption.REPLACE_EXISTING);
+  }
+
+  /**
+   * Takes in list of directory paths as strings and removes each directory even when they are
+   * empty.
+   *
+   * @param directories
+   * @throws IOException
+   */
+  public void removeAllDirectories(List<String> directories)
+      throws IOException, InvalidParameterException {
+    if (directories == null || directories.size() == 0) {
+      throw new InvalidParameterException(directories);
+    }
+
+    for (String dir : directories) {
+      removeDirectory(new File(dir));
+    }
+  }
+
+  /**
+   * Recursively delete a directory or file and all its contents
+   *
+   * @param directoryToBeDeleted
+   * @return boolean value indicating success or failure
+   * @throws InvalidParameterException
+   */
+  public boolean removeDirectory(File directoryToBeDeleted) throws InvalidParameterException {
+    if (directoryToBeDeleted == null) {
+      throw new InvalidParameterException(directoryToBeDeleted);
+    }
+
+    File[] allContents = directoryToBeDeleted.listFiles();
+    if (allContents != null) {
+      for (File file : allContents) {
+        removeDirectory(file);
+      }
+    }
+    return directoryToBeDeleted.delete();
   }
 }
