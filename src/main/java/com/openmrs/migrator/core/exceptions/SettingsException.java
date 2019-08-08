3df863edc1a858ebf43c4233df3fec8510a0e997
k-joseph <kaweesi@jembi.org>
2019-08-08 12:37:43 +0300

    MEPTS-100: added serialVersionUID to SettingsException

--- src/main/java/com/openmrs/migrator/core/exceptions/SettingsException.java
+++ src/main/java/com/openmrs/migrator/core/exceptions/SettingsException.java
@@ -2,6 +2,8 @@ package com.openmrs.migrator.core.exceptions;
 
 public class SettingsException extends Exception {
 
+  private static final long serialVersionUID = -7692114236102622223L;
+
   public SettingsException(Throwable e) {
     super("SettingsException: " + e.getMessage(), e);
   }
