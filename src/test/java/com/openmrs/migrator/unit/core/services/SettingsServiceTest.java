ff965cf5e4616492f33ecae72722484cf4a98827
k-joseph <kaweesi@jembi.org>
2019-10-17 16:05:03 +0300

    MEPTS-185: added more settingsService tests and removed unused controller

--- src/test/java/com/openmrs/migrator/unit/core/services/SettingsServiceTest.java
+++ src/test/java/com/openmrs/migrator/unit/core/services/SettingsServiceTest.java
@@ -2,7 +2,17 @@ package com.openmrs.migrator.unit.core.services;
 
 import com.openmrs.migrator.core.exceptions.SettingsException;
 import com.openmrs.migrator.core.services.SettingsService;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,12 +23,27 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@Ignore // it passes locally but line 20 fails on travis
 public class SettingsServiceTest {
 
   @Autowired private SettingsService settingsService;
 
-  @Test
+  private Path tempSettingsFile;
+
+  @Before
+  public void setUp() {
+    tempSettingsFile =
+        Paths.get(
+            System.getProperty("java.io.tmpdir")
+                + File.separator
+                + SettingsService.SETTINGS_PROPERTIES);
+  }
+
+  @After
+  public void cleanUp() throws IOException {
+    Files.delete(tempSettingsFile);
+  }
+
+  @Ignore
   public void initializeKettleEnvironment() throws SettingsException {
     Assert.assertNull(EnvUtil.getSystemProperty(SettingsService.DB_TEST_CONNECTION));
     Assert.assertNull(EnvUtil.getSystemProperty(SettingsService.SOURCE_DB));
@@ -38,5 +63,39 @@ public class SettingsServiceTest {
     Assert.assertEquals("codecode", EnvUtil.getSystemProperty(SettingsService.DB_PASS));
     Assert.assertEquals(
         "./input", EnvUtil.getSystemProperty(SettingsService.DBS_BACKUPS_DIRECTORY));
+  }
+
+  @Test
+  public void addSettingToConfigFile() throws Exception {
+    String source =
+        SettingsServiceTest.class
+            .getClassLoader()
+            .getResource(SettingsService.SETTINGS_PROPERTIES)
+            .getPath();
+    Files.copy(Paths.get(source), tempSettingsFile);
+    settingsService.addSettingToConfigFile(
+        tempSettingsFile, "TEST_SETTING", 7, "nothing_important_really");
+    List<String> lines = Files.readAllLines(tempSettingsFile);
+    Assert.assertEquals("TEST_SETTING=nothing_important_really", lines.get(6));
+    Assert.assertEquals("EPTS_DATABASES_DIRECTORY=./input", lines.get(7));
+  }
+
+  @Test
+  public void fillConfigFile() throws Exception {
+    Map<String, String> connDB = new HashMap<>();
+    connDB.put(SettingsService.DB_TEST_CONNECTION, "false");
+    connDB.put(SettingsService.DB_USER, "user");
+    connDB.put(SettingsService.DB_PASS, "pass");
+    connDB.put(SettingsService.DB_HOST, "localhost");
+    connDB.put(SettingsService.DB_PORT, "3306");
+    connDB.put(SettingsService.DBS_BACKUPS_DIRECTORY, "./input");
+    settingsService.fillConfigFile(tempSettingsFile, connDB);
+    List<String> lines = Files.readAllLines(tempSettingsFile);
+    Assert.assertEquals(SettingsService.DB_TEST_CONNECTION + "=false", lines.get(0));
+    Assert.assertEquals(SettingsService.DB_USER + "=user", lines.get(1));
+    Assert.assertEquals(SettingsService.DB_PASS + "=pass", lines.get(2));
+    Assert.assertEquals(SettingsService.DB_HOST + "=localhost", lines.get(3));
+    Assert.assertEquals(SettingsService.DB_PORT + "=3306", lines.get(4));
+    Assert.assertEquals(SettingsService.DBS_BACKUPS_DIRECTORY + "=./input", lines.get(5));
   }
 }
