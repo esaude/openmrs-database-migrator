b563dcc73a245882ea061041e5df9c4ad7603bee
Yassin Nurmahomed <yassin.nurmahomed@jembi.org>
2020-06-12 14:23:11 +0200

    Add host option to form import

--- src/main/resources/config/form-import.sh
+++ src/main/resources/config/form-import.sh
@@ -5,5 +5,6 @@
 #"$2" is for password
 #"$3" is for database name
 #"$4" is for mysql form data dump
+#"$5" is for mysql host
 
-mysql -u "$1" -p"$2" "$3" < "$4"
+mysql -h "$5" -u "$1" -p"$2" "$3" < "$4"
